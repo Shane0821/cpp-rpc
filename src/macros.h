ce4c44ad35136e2f447e9754135fb083ec341157
Shane <143796207+Shane0821@users.noreply.github.com>
2024-09-15 22:38:14 +0800

    add macros

---
+++ src/macros.h
@@ -0,0 +1,78 @@
+#ifndef _RPC_MACROS_H
+#define _RPC_MACROS_H
+
+#include <llbc.h>
+
+#define COND_RET_TLOG(condition, retCode, ...) \
+    {                                          \
+        if (condition) {                       \
+            LLOG_TRACE(__VA_ARGS__);           \
+            return retCode;                    \
+        }                                      \
+    }
+
+#define COND_RET_ILOG(condition, retCode, ...) \
+    {                                          \
+        if (condition) {                       \
+            LLOG_INFO(__VA_ARGS__);            \
+            return retCode;                    \
+        }                                      \
+    }
+
+#define COND_RET_WLOG(condition, retCode, ...) \
+    {                                          \
+        if (condition) {                       \
+            LLOG_WARN(__VA_ARGS__);            \
+            return retCode;                    \
+        }                                      \
+    }
+
+#define COND_RET_ELOG(condition, retCode, ...) \
+    {                                          \
+        if (condition) {                       \
+            LLOG_ERROR(__VA_ARGS__);           \
+            return retCode;                    \
+        }                                      \
+    }
+
+#define COND_EXP_ELOG(condition, expr, ...) \
+    {                                       \
+        if (condition) [[unlikely]] {       \
+            LLOG_ERROR(__VA_ARGS__);        \
+            expr;                           \
+        }                                   \
+    }
+
+#define CO_COND_RET_TLOG(condition, retCode, ...) \
+    {                                             \
+        if (condition) {                          \
+            LLOG_TRACE(__VA_ARGS__);              \
+            co_return retCode;                    \
+        }                                         \
+    }
+
+#define CO_COND_RET_ILOG(condition, retCode, ...) \
+    {                                             \
+        if (condition) {                          \
+            LLOG_INFO(__VA_ARGS__);               \
+            co_return retCode;                    \
+        }                                         \
+    }
+
+#define CO_COND_RET_WLOG(condition, retCode, ...) \
+    {                                             \
+        if (condition) {                          \
+            LLOG_WARN(__VA_ARGS__);               \
+            co_return retCode;                    \
+        }                                         \
+    }
+
+#define CO_COND_RET_ELOG(condition, retCode, ...) \
+    {                                             \
+        if (condition) {                          \
+            LLOG_ERROR(__VA_ARGS__);              \
+            co_return retCode;                    \
+        }                                         \
+    }
+
+#endif // _RPC_MACROS_H
