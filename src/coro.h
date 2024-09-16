aadb033b37ce1f7ecb6f977e37d41cd7fe153df0
Shane <143796207+Shane0821@users.noreply.github.com>
2024-09-16 22:54:20 +0800

    add comment; add handle check

--- src/coro.h
+++ src/coro.h
@@ -12,26 +12,39 @@ class RpcCoro {
     struct promise_type {
         int unique_id;
 
+        // Called to get the return object of the coroutine
         auto get_return_object() { return RpcCoro{handle_type::from_promise(*this)}; }
 
+        // Define when the coroutine initially suspends
         auto initial_suspend() { return std::suspend_never{}; }
+
+        // Define behavior at the end of the coroutine
         auto final_suspend() noexcept { return std::suspend_always{}; }
+
+        // Handle exceptions
         void unhandled_exception() { std::terminate(); }
+
+        // Return void from the coroutine
         void return_void() {}
+
+        // Yield value
         auto yield_value() { return std::suspend_always{}; }
     };
 
     RpcCoro(RpcCoro const &) = delete;
     RpcCoro(RpcCoro &&rhs) : coro_handle(rhs.coro_handle) { rhs.coro_handle = nullptr; }
 
-    ~RpcCoro() {}
+    ~RpcCoro() {
+        if (coro_handle) coro_handle.destroy();
+    }
 
     int get_id() const { return coro_handle.promise().unique_id; }
     void resume() { coro_handle.resume(); }
-    auto yield() { return coro_handle.promise().yield_value(); }
     void cancel() {
-        coro_handle.destroy();
-        coro_handle = nullptr;
+        if (coro_handle) {
+            coro_handle.destroy();
+            coro_handle = nullptr;
+        }
     }
 
    private:
