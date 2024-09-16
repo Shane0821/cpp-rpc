f797cab8cb4bd49c7df0afafebef4b5bf6131273
Shane <143796207+Shane0821@users.noreply.github.com>
2024-09-16 12:12:24 +0800

    add connection component

---
+++ src/conn_comp.cpp
@@ -0,0 +1,58 @@
+#include "conn_comp.h"
+
+#include "macros.h"
+
+ConnComp::ConnComp() : llbc::LLBC_Component() {}
+
+bool ConnComp::OnInit(bool &initFinished) {
+    LLOG_TRACE("Service create!");
+    return true;
+}
+
+void ConnComp::OnDestroy(bool &destroyFinished) { LLOG_TRACE("Service destroy!"); }
+
+void ConnComp::OnSessionCreate(const llbc::LLBC_SessionInfo &sessionInfo) {
+    LLOG_TRACE("Session Create: %s", sessionInfo.ToString().c_str());
+}
+
+void ConnComp::OnSessionDestroy(const llbc::LLBC_SessionDestroyInfo &destroyInfo) {
+    LLOG_TRACE("Session Destroy, info: %s", destroyInfo.ToString().c_str());
+}
+
+void ConnComp::OnAsyncConnResult(const llbc::LLBC_AsyncConnResult &result) {
+    LLOG_TRACE("Async-Conn result: %s", result.ToString().c_str());
+}
+
+void ConnComp::OnUnHandledPacket(const llbc::LLBC_Packet &packet) {
+    LLOG_TRACE("Unhandled packet, sessionId: %d, opcode: %d, payloadLen: %ld",
+               packet.GetSessionId(), packet.GetOpcode(), packet.GetPayloadLength());
+}
+
+void ConnComp::OnProtoReport(const llbc::LLBC_ProtoReport &report) {
+    LLOG_TRACE("Proto report: %s", report.ToString().c_str());
+}
+
+void ConnComp::OnUpdate() {
+    llbc::LLBC_Packet sendPacket;
+    while (sendQueue_.pop(sendPacket)) {
+        LLOG_TRACE("sendPacket: %s", sendPacket.ToString().c_str());
+        auto ret = GetService()->Send(&sendPacket);
+        if (ret != LLBC_OK) {
+            LLOG_ERROR("Send packet failed, err: %s", llbc::LLBC_FormatLastError());
+        }
+    }
+}
+
+void ConnComp::OnRecvPacket(llbc::LLBC_Packet &packet) {
+    LLOG_TRACE("OnRecvPacket: %s", packet.ToString().c_str());
+    llbc::LLBC_Packet recvPacket;
+    recvPacket.SetHeader(packet.GetSessionId(), packet.GetOpcode(), 0);
+    recvPacket.SetPayload(packet.DetachPayload());
+    recvQueue_.emplace(recvPacket);
+}
+
+int ConnComp::PushPacket(llbc::LLBC_Packet &sendPacket) {
+    if (sendQueue_.emplace(sendPacket)) return LLBC_OK;
+
+    return LLBC_FAILED;
+}
---
+++ src/conn_comp.h
@@ -0,0 +1,33 @@
+#include <llbc.h>
+#include <spsc_queue.h>
+
+// 连接管理组件
+class ConnComp : public llbc::LLBC_Component {
+   public:
+    ConnComp();
+    virtual ~ConnComp() {}
+
+    virtual bool OnInit(bool &initFinished);
+    virtual void OnDestroy(bool &destroyFinished);
+    virtual void OnUpdate();
+
+    // monitor network event
+    virtual void OnSessionCreate(const llbc::LLBC_SessionInfo &sessionInfo);
+    virtual void OnSessionDestroy(const llbc::LLBC_SessionDestroyInfo &destroyInfo);
+    virtual void OnAsyncConnResult(const llbc::LLBC_AsyncConnResult &result);
+    virtual void OnUnHandledPacket(const llbc::LLBC_Packet &packet);
+    virtual void OnProtoReport(const llbc::LLBC_ProtoReport &report);
+
+    // send packet
+    int PushPacket(llbc::LLBC_Packet &sendPacket);
+    // recv packet
+    llbc::LLBC_Packet *PopPacket();
+    // callback when recv packet
+    void OnRecvPacket(llbc::LLBC_Packet &packet);
+
+    static constexpr int MAX_BUFFER_SIZE = 1024;
+
+   private:
+    SPSCQueue<llbc::LLBC_Packet, MAX_BUFFER_SIZE> sendQueue_;
+    SPSCQueue<llbc::LLBC_Packet, MAX_BUFFER_SIZE> recvQueue_;
+};
