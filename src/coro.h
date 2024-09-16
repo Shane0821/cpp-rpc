b8b77586fb37ee6822291d8e4d9beceef31a8752
Shane <143796207+Shane0821@users.noreply.github.com>
2024-09-16 23:07:00 +0800

    upd

--- src/coro.h
+++ src/coro.h
@@ -13,10 +13,12 @@ class RpcCoro {
         int unique_id;
 
         // Called to get the return object of the coroutine
-        auto get_return_object() { return RpcCoro{handle_type::from_promise(*this)}; }
+        auto get_return_object() noexcept {
+            return RpcCoro{handle_type::from_promise(*this)};
+        }
 
         // Define when the coroutine initially suspends
-        auto initial_suspend() { return std::suspend_never{}; }
+        auto initial_suspend() noexcept { return std::suspend_never{}; }
 
         // Define behavior at the end of the coroutine
         auto final_suspend() noexcept { return std::suspend_always{}; }
@@ -25,22 +27,36 @@ class RpcCoro {
         void unhandled_exception() { std::terminate(); }
 
         // Return void from the coroutine
-        void return_void() {}
+        void return_void() noexcept {}
 
         // Yield value
-        auto yield_value() { return std::suspend_always{}; }
+        auto yield_value() noexcept { return std::suspend_always{}; }
+    };
+
+    struct GetHandleAwaiter {
+        bool await_ready() const noexcept { return false; }
+
+        bool await_suspend(std::coroutine_handle<promise_type> handle) {
+            handle_ = handle.address();
+            return false;
+        }
+
+        void *await_resume() noexcept { return handle_; }
+
+        void *handle_;
     };
 
     RpcCoro(RpcCoro const &) = delete;
-    RpcCoro(RpcCoro &&rhs) : coro_handle(rhs.coro_handle) { rhs.coro_handle = nullptr; }
-
+    RpcCoro(RpcCoro &&rhs) noexcept : coro_handle(rhs.coro_handle) {
+        rhs.coro_handle = nullptr;
+    }
     ~RpcCoro() {
         if (coro_handle) coro_handle.destroy();
     }
 
-    int get_id() const { return coro_handle.promise().unique_id; }
-    void resume() { coro_handle.resume(); }
-    void cancel() {
+    int get_id() const noexcept { return coro_handle.promise().unique_id; }
+    void resume() noexcept { coro_handle.resume(); }
+    void cancel() noexcept {
         if (coro_handle) {
             coro_handle.destroy();
             coro_handle = nullptr;
@@ -48,7 +64,12 @@ class RpcCoro {
     }
 
    private:
-    RpcCoro(handle_type h) : coro_handle(h) {}
+    /**
+     * This is set to private, to make sure that:
+     * 1. only the coroutine infrastructure (via get_return_object) can create instances
+     * 2. the promise type is tightly coupled with the coroutine's return object
+     */
+    RpcCoro(handle_type h) noexcept : coro_handle(h) {}
 
     handle_type coro_handle;
 };
