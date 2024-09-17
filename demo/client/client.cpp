252f8d76f67e3d9705c88b105201045b65783825
Shane <143796207+Shane0821@users.noreply.github.com>
2024-09-17 21:53:04 +0800

    init client and server

--- demo/client/client.cpp
+++ demo/client/client.cpp
@@ -1,5 +1,72 @@
-#include "echo_client_impl.h"
+#include <llbc.h>
+#include <signal.h>
 
-int main() {
+#include <iostream>
+
+#include "channel.h"
+#include "conn_mgr.h"
+#include "coro_mgr.h"
+#include "echo.pb.h"
+#include "echo_client_impl.h"
+#include "macros.h"
+#include "service_mgr.h"
+
+bool stop = false;
+static const std::string CLIENT_LLOG_CONF_PATH =
+    "../../config/client_log.cfg";  // change path
+
+void signalHandler(int signum) {
+    std::cout << "INTERRUPT SIGNAL [" << signum << "] received.\n";
+    stop = true;
+}
+
+void mainloop() {
+    // 创建 rpc req & resp
+    echo::EchoRequest req;
+    echo::EchoResponse rsp;
+
+    while (!stop) {
+        std::string input;
+        COND_EXP(!(std::cin >> input), break);  // 手动阻塞
+        COND_EXP(input == "exit", break);
+        // req.set_msg(input);
+        // auto ret = echo::   ::DemoServiceStub::Echo(0UL, req, &rsp);
+        // COND_EXP_ELOG(ret != 0, continue, "call Stub::method failed|ret:%d", ret);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    // 注册信号 SIGINT 和信号处理程序
+    ::signal(SIGINT, signalHandler);
+
+    // 初始化 llbc 库
+    llbc::LLBC_Startup();
+    LLBC_Defer(llbc::LLBC_Cleanup());
+
+    // 初始化日志
+    auto ret = LLBC_LoggerMgrSingleton->Initialize(CLIENT_LLOG_CONF_PATH);
+    if (ret == LLBC_FAILED) {
+        // fmt::print("Initialize logger failed|path:{}|error: {}\n",
+        // CLIENT_LLOG_CONF_PATH,
+        //            llbc::LLBC_FormatLastError());
+        return EXIT_FAILURE;
+    }
+
+    // 初始化连接管理器
+    ret = ConnMgr::GetInst().Init();
+    COND_RET_ELOG(ret != LLBC_OK, -1, "init ConnMgr failed|error: %s",
+                  llbc::LLBC_FormatLastError());
+
+    // RpcCoroMgr::GetInst().UseCoro(false);  // 客户端默认不用协程，一直阻塞等待回包
+
+    // 协程方案, 在新协程中 call rpc
+    ret = RpcServiceMgr::GetInst().Init(&ConnMgr::GetInst());
+    COND_RET_ELOG(ret != 0, ret, "RpcServiceMgr init failed|ret:%d", ret);
+    RpcServiceMgr::GetInst().RegisterChannel("127.0.0.1", 6688);
+
+    mainloop();
+
+    LLOG_TRACE("client stop");
+
     return 0;
 }
--- demo/service/service.cpp
+++ demo/service/service.cpp
@@ -1,5 +1,71 @@
-#include "echo_service_impl.h"
+#include <llbc.h>
+#include <signal.h>
 
-int main() {
+#include <iostream>
+
+#include "conn_mgr.h"
+#include "macros.h"
+#include "coro_mgr.h"
+#include "server.h"
+#include "service_mgr.h"
+
+#define REGISTER_RPC_CHANNEL(ip, port)                                                   \
+    {                                                                                    \
+        bool _succ = RpcServiceMgr::GetInst().RegisterChannel(ip, port);                 \
+        COND_RET_ELOG(!_succ, EXIT_FAILURE, "register channel failed|ip:%s|port:%d", ip, \
+                      port);                                                             \
+        LLOG_INFO("register channel succ|ip:%s|port:%d", ip, port);                      \
+    }
+
+static const std::string SERVER_LLOG_CONF_PATH =
+    "../../config/server_log.cfg";  // change path
+
+void signalHandler(int signum) {
+    std::cout << "INTERRUPT SIGNAL [" << signum << "] received.\n";
+    RpcServer::GetInst().Stop();
+}
+
+int main(int argc, char *argv[]) {
+    // 注册信号 SIGINT 和信号处理程序
+    signal(SIGINT, signalHandler);
+
+    // 初始化 llbc 库
+    llbc::LLBC_Startup();
+    // LLBC_HookProcessCrash();
+    LLBC_Defer(llbc::LLBC_Cleanup());
+
+    // 初始化日志
+    auto ret = LLBC_LoggerMgrSingleton->Initialize(SERVER_LLOG_CONF_PATH);
+    if (ret == LLBC_FAILED) {
+        // fmt::print("Initialize logger failed|path:{}|error: {}\n",
+        // SERVER_LLOG_CONF_PATH,
+        //            LLBC_FormatLastError());
+        return -1;
+    }
+    LLOG_TRACE("Hello Server!");
+
+    ret = ConnMgr::GetInst().Init();
+    COND_RET_ELOG(ret != 0, ret, "ConnMgr init failed|ret:%d", ret);
+
+    // 启动 rpc 服务
+    COND_RET_ELOG(ConnMgr::GetInst().StartRpcService("127.0.0.1", 6688) != LLBC_OK, -1,
+                  "ConnMgr StartRpcService failed");
+
+    ret = RpcServiceMgr::GetInst().Init(&ConnMgr::GetInst());
+    COND_RET_ELOG(ret != 0, ret, "RpcServiceMgr init failed|ret:%d", ret);
+    // bool succ = RpcServiceMgr::GetInst().AddService(nullptr);
+    // COND_RET_ELOG(!succ, EXIT_FAILURE, "add service failed");
+
+    REGISTER_RPC_CHANNEL("127.0.0.1", 6688);
+    REGISTER_RPC_CHANNEL("127.0.0.1", 6699);
+
+#undef REGISTER_RPC_CHANNEL
+
+    RpcServer::GetInst().Init();
+    COND_RET_ELOG(ret != 0, ret, "RpcServer init failed|ret:%d", ret);
+
+    // RpcCoroMgr::GetInst().UseCoro(true);  // 服务端启用协程来处理请求
+    RpcServer::GetInst().Run();
+
     return 0;
 }
