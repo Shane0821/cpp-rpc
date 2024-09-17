c75c2f5bd79886d361e04733c3d331c2e170d15f
Shane <143796207+Shane0821@users.noreply.github.com>
2024-09-17 11:30:31 +0800

    strictly follow singleton pattern

--- src/controller.h
+++ src/controller.h
@@ -4,17 +4,24 @@
 #include <google/protobuf/service.h>
 #include <singleton.h>
 
-class RpcController : public ::google::protobuf::RpcController, public Singleton<RpcController> {
-public:
-    RpcController() = default;
-    ~RpcController() { }
-    virtual void Reset() { }
+class RpcController : public ::google::protobuf::RpcController,
+                      public Singleton<RpcController> {
+    friend class Singleton<RpcController>;
+
+   public:
+    ~RpcController() = default;
+    virtual void Reset() {}
     virtual bool Failed() const { return false; }
     virtual std::string ErrorText() const { return ""; }
-    virtual void StartCancel() { }
-    virtual void SetFailed(const std::string & /* reason */) { }
+    virtual void StartCancel() {}
+    virtual void SetFailed(const std::string& /* reason */) {}
     virtual bool IsCanceled() const { return false; }
-    virtual void NotifyOnCancel(::google::protobuf::Closure * /* callback */) { }
+    virtual void NotifyOnCancel(::google::protobuf::Closure* /* callback */) {}
+
+   protected:
+    RpcController() = default;
+    RpcController(const RpcController&) = delete;
+    RpcController& operator=(const RpcController&) = delete;
 };
 
 #endif  // _RPC_CONTROLLER_H
