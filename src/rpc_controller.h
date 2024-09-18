dd8686d5ff3c4a27f6deea6cb28f06509b1b7505
Shane <143796207+Shane0821@users.noreply.github.com>
2024-09-18 20:41:03 +0800

    upd controller: add failed and error text

--- src/rpc_controller.h
+++ src/rpc_controller.h
@@ -2,26 +2,25 @@
 #define _RPC_CONTROLLER_H
 
 #include <google/protobuf/service.h>
-#include <singleton.h>
 
-class RpcController : public ::google::protobuf::RpcController,
-                      public Singleton<RpcController> {
-    friend class Singleton<RpcController>;
-
+class RpcController : public ::google::protobuf::RpcController {
    public:
+    RpcController() = default;
     ~RpcController() = default;
     virtual void Reset() {}
-    virtual bool Failed() const { return false; }
-    virtual std::string ErrorText() const { return ""; }
+    virtual bool Failed() const { return isFailed_; };
+    virtual std::string ErrorText() const { return errorText_; };
     virtual void StartCancel() {}
-    virtual void SetFailed(const std::string& /* reason */) {}
+    virtual void SetFailed(const std::string& reason) {
+        isFailed_ = true;
+        errorText_ = reason;
+    };
     virtual bool IsCanceled() const { return false; }
     virtual void NotifyOnCancel(::google::protobuf::Closure* /* callback */) {}
 
-   protected:
-    RpcController() = default;
-    RpcController(const RpcController&) = delete;
-    RpcController& operator=(const RpcController&) = delete;
+   private:
+    bool isFailed_ = false;
+    std::string errorText_;
 };
 
 #endif  // _RPC_CONTROLLER_H
