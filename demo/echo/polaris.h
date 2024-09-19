682add1af2610aa30d49db0fc125c309aafa6247
Shane <143796207+Shane0821@users.noreply.github.com>
2024-09-19 18:30:50 +0800

    fix compile error

--- demo/echo/polaris.h
+++ demo/echo/polaris.h
@@ -1,7 +1,11 @@
 #include <string>
 #include <unordered_map>
 
-std::unordered_map<std::string, std::pair<const char *, int>> NameRegistry = {
-    {"echo.EchoService.Echo", {"127.0.0.1", 6688}},
-    {"echo.EchoService.RelayEcho", {"127.0.0.1", 6699}}
+struct ServiceAddress {
+    const char *ip;
+    int port;
 };
+
+std::unordered_map<std::string, ServiceAddress> NameRegistry = {
+    {"echo.EchoService.Echo", {"127.0.0.1", 6688}},
+    {"echo.EchoService.RelayEcho", {"127.0.0.1", 6699}}};
