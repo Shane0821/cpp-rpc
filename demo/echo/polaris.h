c16a2d6a2bce407d75024f49438745c6729e7ddb
Shane <143796207+Shane0821@users.noreply.github.com>
2024-09-19 18:02:56 +0800

    add name registry

---
+++ demo/echo/polaris.h
@@ -0,0 +1,7 @@
+#include <string>
+#include <unordered_map>
+
+std::unordered_map<std::string, std::pair<const char *, int>> NameRegistry = {
+    {"echo.EchoService.Echo", {"127.0.0.1", 6688}},
+    {"echo.EchoService.RelayEcho", {"127.0.0.1", 6699}},
+}
