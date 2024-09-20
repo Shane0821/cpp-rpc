bc59fcf7ed0d0ee7d6d9c31fb7d21e4e1d823460
Shane <143796207+Shane0821@users.noreply.github.com>
2024-09-20 16:44:18 +0800

    add call method logic

--- src/rpc_channel.cpp
+++ src/rpc_channel.cpp
@@ -1,6 +1,7 @@
 #include "rpc_channel.h"
 
 #include "rpc_conn_mgr.h"
+#include "rpc_controller.h"
 #include "rpc_macros.h"
 
 RpcChannel::~RpcChannel() { connMgr_->CloseSession(sessionId_); }
@@ -36,33 +37,42 @@ int RpcChannel::PkgHead::ToPacket(llbc::LLBC_Packet &packet) const {
 }
 
 const std::string &RpcChannel::PkgHead::ToString() const {
-    static std::string buffer(MAX_BUFFER_SIZE, '\0');
+    static std::string buffer;
+    std::string().swap(buffer);
     ::snprintf(buffer.data(), MAX_BUFFER_SIZE, "src:%u|dst:%u|uid:%lu|seq:%lu|cmd:0x%08X",
                src, dst, uid, seq, cmd);
     return buffer;
 }
 
-void RpcChannel::CallMethod(const ::google::protobuf::MethodDescriptor *method,
-                            ::google::protobuf::RpcController *controller,
-                            const ::google::protobuf::Message *request,
-                            ::google::protobuf::Message *response,
-                            ::google::protobuf::Closure *done) {
-    LLOG_ERROR("NO IMPLEMENTATION|Please use XXServiceStub::Method to start RPC");
-}
+void RpcChannel::CallMethod(
+    const ::google::protobuf::MethodDescriptor *method,
+    ::google::protobuf::RpcController *controller,
+    const ::google::protobuf::Message *request,
+    ::google::protobuf::Message *response,  // handled by coroutine context
+    ::google::protobuf::Closure *done) {
+    LLOG_TRACE("CallMethod!");
 
-int RpcChannel::Send(const PkgHead &pkg_head,
-                     const ::google::protobuf::Message &message) {
-    llbc::LLBC_Packet packet;
-    packet.SetHeader(0, RpcOpCode::RpcReq, 0);
-    packet.SetSessionId(sessionId_);
+    RpcController *rpcController = dynamic_cast<RpcController *>(controller);
+    COND_RET_ELOG(rpcController == nullptr, , "controller is not RpcController");
 
-    int ret = pkg_head.ToPacket(packet);
-    COND_RET_ELOG(ret != 0, ret, "pkg_head.ToPacket failed|ret:%d", ret);
-    ret = packet.Write(message);
-    COND_RET_ELOG(ret != 0, ret, "packet.Write message failed|ret:%d", ret);
-    LLOG_DEBUG("send data|message: %s|packet: %s", message.ShortDebugString().c_str(),
-               packet.ToString().c_str());
+    response->Clear();
 
-    connMgr_->SendPacket(packet);
-    return 0;
+    llbc::LLBC_Packet *sendPacket =
+        llbc::LLBC_ThreadSpecObjPool::GetSafeObjPool()->Acquire<llbc::LLBC_Packet>();
+    sendPacket->SetHeader(0, RpcOpCode::RpcReq, 0);
+    sendPacket->SetSessionId(sessionId_);
+
+    int ret = rpcController->GetPkgHead().ToPacket(*sendPacket);
+    COND_RET_ELOG(ret != LLBC_OK, , "pkg_head.ToPacket failed|ret:%d", ret);
+    ret = sendPacket->Write(*request);
+    COND_RET_ELOG(ret != LLBC_OK, , "packet.Write message failed|ret:%d", ret);
+    LLOG_DEBUG("send data|message: %s|packet: %s", request->ShortDebugString().c_str(),
+               sendPacket->ToString().c_str());
+
+    // send packet via conn_mgr
+    ret = RpcConnMgr::GetInst().SendPacket(*sendPacket);
+    COND_RET_ELOG(ret != LLBC_OK, , "PushPacket failed, ret: %s",
+                  llbc::LLBC_FormatLastError());
+
+    LLOG_TRACE("Waiting!");
 }
