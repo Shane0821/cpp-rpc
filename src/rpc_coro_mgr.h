202008b646d44164f41e417b6f855483bc93993b
Shane <143796207+Shane0821@users.noreply.github.com>
2024-09-21 11:54:55 +0800

    store coro uid inside context; upd timeout handle logic; rename variables

--- src/rpc_coro_mgr.h
+++ src/rpc_coro_mgr.h
@@ -2,6 +2,7 @@
 #define _RPC_CORO_MGR_H_
 
 #include <google/protobuf/message.h>
+#include <google/protobuf/text_format.h>
 #include <llbc.h>
 
 #include <coroutine>
@@ -19,7 +20,8 @@ class RpcCoroMgr : public Singleton<RpcCoroMgr> {
 
     struct context {
         int session_id = 0;
-        llbc::sint64 timeoutTime_;
+        coro_uid_type coro_uid = 0UL;
+        llbc::sint64 timeout_time;
         std::coroutine_handle<RpcCoro::promise_type> handle = nullptr;
         ::google::protobuf::Message *rsp = nullptr;
         RpcController *controller = nullptr;
@@ -27,7 +29,7 @@ class RpcCoroMgr : public Singleton<RpcCoroMgr> {
 
     struct contextCmp {
         bool operator()(context &a, context &b) {
-            return a.timeoutTime_ < b.timeoutTime_;
+            return a.timeout_time < b.timeout_time;
         }
     };
 
@@ -43,8 +45,8 @@ class RpcCoroMgr : public Singleton<RpcCoroMgr> {
         return ++coro_uid_generator_ == 0UL ? ++coro_uid_generator_ : coro_uid_generator_;
     }
 
-    bool AddCoroContext(coro_uid_type coro_uid, context ctx) noexcept {
-        return suspended_contexts_.insert({coro_uid, ctx}).second;
+    bool AddCoroContext(context ctx) noexcept {
+        return suspended_contexts_.insert({ctx.coro_uid, ctx}).second;
     }
 
     context PopCoroContext(coro_uid_type coro_uid) {
@@ -62,15 +64,17 @@ class RpcCoroMgr : public Singleton<RpcCoroMgr> {
         llbc::sint64 now = llbc::LLBC_GetMilliseconds();
         while (!coroHeap_.empty()) {
             auto &top = coroHeap_.top();
-            if (top.timeoutTime_ > now) {
+            if (top.timeout_time > now) {
                 break;
             }
-            auto ctx = PopCoroContext(top.session_id);
-            if (ctx.handle) {
-                ctx.handle.destroy();
-            }
-            ctx.controller->SetFailed("rpc coro timeout");
             coroHeap_.pop();
+            auto ctx = PopCoroContext(top.coro_uid);
+            if (ctx.handle == nullptr) {
+                continue;
+            }
+            ctx.controller->SetFailed("coro timeout");
+            google::protobuf::TextFormat::ParseFromString("coro timeout", ctx.rsp);
+            ctx.handle.resume();
         }
     }
 
@@ -90,8 +94,7 @@ class RpcCoroMgr : public Singleton<RpcCoroMgr> {
 // Used with the co_await keyword in coroutines.
 struct RpcSaveContextAwaiter {
    public:
-    RpcSaveContextAwaiter(RpcCoroMgr::coro_uid_type coro_uid, RpcCoroMgr::context context)
-        : coro_uid_(coro_uid), context_(context) {}
+    RpcSaveContextAwaiter(RpcCoroMgr::context context) : context_(context) {}
 
     bool await_ready() const noexcept {
         // Always returns false, meaning the coroutine will always suspend when this
@@ -101,10 +104,10 @@ struct RpcSaveContextAwaiter {
 
     // Called when the coroutine is suspended
     decltype(auto) await_suspend(std::coroutine_handle<RpcCoro::promise_type> handle) {
-        LLOG_INFO("suspend coro|coro_uid:%lu|%p|rsp:%p", coro_uid_, handle.address(),
-                  context_.rsp);
+        LLOG_INFO("suspend coro|coro_uid:%lu|%p|rsp:%p", context_.coro_uid,
+                  handle.address(), context_.rsp);
         context_.handle = handle;
-        RpcCoroMgr::GetInst().AddCoroContext(coro_uid_, context_);
+        RpcCoroMgr::GetInst().AddCoroContext(context_);
         // false: Immediate resumption after suspension.
         // true: Suspension until explicitly resumed by external logic.
         return true;
@@ -114,7 +117,6 @@ struct RpcSaveContextAwaiter {
     void await_resume() {}
 
    private:
-    RpcCoroMgr::coro_uid_type coro_uid_ = 0UL;
     RpcCoroMgr::context context_;
 };
 
