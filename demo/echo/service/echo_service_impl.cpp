a3266b1126a8f686399a8f3aa4ab819e2f914a6f
Shane <143796207+Shane0821@users.noreply.github.com>
2024-09-22 20:28:57 +0800

    upd service impl

--- demo/echo/service/echo_service_impl.cpp
+++ demo/echo/service/echo_service_impl.cpp
@@ -2,10 +2,12 @@
 
 #include <llbc.h>
 
+#include "echo_service_stub.h"
+#include "polaris.h"
 #include "rpc_channel.h"
 #include "rpc_conn_mgr.h"
+#include "rpc_controller.h"
 #include "rpc_coro.h"
-#include "echo_client_impl.h"
 
 using namespace llbc;
 
@@ -13,52 +15,56 @@ void EchoServiceImpl::Echo(::google::protobuf::RpcController *controller,
                            const ::echo::EchoRequest *request,
                            ::echo::EchoResponse *response,
                            ::google::protobuf::Closure *done) {
-    // LLBC_Sleep(5000); timeout test
+    // LLBC_Sleep(10000); timeout test
     LLOG_INFO("received, msg:%s\n", request->msg().c_str());
     response->set_msg(std::string(" Echo >>>>>>> ") + request->msg());
     done->Run();
 }
 
-RpcCoro InnerCallMeathod(::google::protobuf::RpcController *controller,
-                         const ::echo::EchoRequest *req, ::echo::EchoResponse *rsp,
-                         ::google::protobuf::Closure *done) {
-    LLOG_INFO("received, msg:%s", req->msg().c_str());
+RpcCoro InnerCallEcho(::google::protobuf::RpcController *controller,
+                      const ::echo::EchoRequest *req, ::echo::EchoResponse *rsp,
+                      ::google::protobuf::Closure *done) {
     // init inner rpc req & rsp
     echo::EchoRequest innerReq;
     innerReq.set_msg("Relay Call >>>>>>" + req->msg());
+    LLOG_INFO("call, msg:%s", innerReq.msg().c_str());
     echo::EchoResponse innerRsp;
-    // create rpc channel
-    RpcChannel *channel = RpcConnMgr::GetInst().CreateRpcChannel("127.0.0.1", 6699);
+
+    // TODO: create channel in advance
+    static auto addr = polaris::NameRegistry[EchoServiceImpl::ECHO_SERVICE_NAME];
+    RpcChannel *channel = RpcConnMgr::GetInst().CreateRpcChannel(addr.ip, addr.port);
     if (!channel) {
-        LLOG_INFO("GetRpcChannel Fail");
-        rsp->set_msg(req->msg() + " ---- inner rpc call server not exist");
-        controller->SetFailed("GetRpcChannel Fail");
+        LLOG_ERROR("CreateRpcChannel for ReplayEcho failed.");
+        rsp->set_msg(req->msg() + " ---- inner rpc call server does not exist");
+        controller->SetFailed("CreateRpcChannel for ReplayEcho Fail");
         done->Run();
         co_return;
     }
 
-    LLOG_INFO("call, msg:%s", innerReq.msg().c_str());
-
     EchoServiceStub stub(channel);
-    // // RpcController cntl(co_await GetHandleAwaiter{});
-    // // co_await stub.Echo(&cntl, &innerReq, &innerRsp, nullptr);
-    // // LLOG_INFO("Recv rsp, status:%s, rsp:%s\n",
-    // //           cntl.Failed() ? cntl.ErrorText().c_str() : "success",
-    // //           innerRsp.msg().c_str());
-    // // if (cntl.Failed()) {
-    // //     controller->SetFailed(cntl.ErrorText());
-    // // }
+    RpcController *inner_controller = new RpcController();
+    inner_controller->SetCoroHandle(co_await GetHandleAwaiter{});
+    co_await stub.Echo(controller, &innerReq, &innerRsp, nullptr);
 
+    LLOG_INFO(
+        "Recv rsp. status:%s, rsp:%s\n",
+        inner_controller->Failed() ? inner_controller->ErrorText().c_str() : "success",
+        innerRsp.msg().c_str());
+    if (inner_controller->Failed()) {
+        controller->SetFailed(inner_controller->ErrorText());
+    }
+
+    // delete channel;
     rsp->set_msg(innerRsp.msg());
     done->Run();
-
+    delete inner_controller;
     co_return;
 }
 
 void EchoServiceImpl::RelayEcho(::google::protobuf::RpcController *controller,
                                 const ::echo::EchoRequest *req, ::echo::EchoResponse *rsp,
                                 ::google::protobuf::Closure *done) {
-    LLOG_TRACE("RECEIVED MSG: %s", req->msg().c_str());
+    LLOG_INFO("received, msg:%s", req->msg().c_str());
 
-    InnerCallMeathod(controller, req, rsp, done);
+    InnerCallEcho(controller, req, rsp, done);
 }
--- demo/echo/service/echo_service_impl.h
+++ demo/echo/service/echo_service_impl.h
@@ -10,4 +10,7 @@ class EchoServiceImpl : public echo::EchoService {
     void RelayEcho(::google::protobuf::RpcController *controller,
                    const ::echo::EchoRequest *request, ::echo::EchoResponse *response,
                    ::google::protobuf::Closure *done) override;
+
+   static constexpr const char *ECHO_SERVICE_NAME = "echo.EchoService.Echo";
+   static constexpr const char *RELAYECHO_SERVICE_NAME = "echo.EchoService.RelayEcho";
 };
