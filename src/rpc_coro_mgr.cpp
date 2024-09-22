291dd5987580a2fe772f8c301b3a783e89527498
Shane <143796207+Shane0821@users.noreply.github.com>
2024-09-22 18:58:19 +0800

    move definition to cpp file

--- src/rpc_coro_mgr.cpp
+++ src/rpc_coro_mgr.cpp
@@ -1,3 +1,49 @@
 #include "rpc_coro_mgr.h"
 
 RpcCoroMgr::coro_uid_type RpcCoroMgr::coro_uid_generator_ = 0UL;
+
+bool RpcCoroMgr::AddCoroContext(context ctx) noexcept {
+    coroHeap_.push(ctx);
+    return suspended_contexts_.insert({ctx.coro_uid, ctx}).second;
+}
+
+void RpcCoroMgr::KillCoro(coro_uid_type coro_uid, const std::string &reason) noexcept {
+    if (auto iter = suspended_contexts_.find(coro_uid);
+        iter != suspended_contexts_.end()) {
+        auto ctx = iter->second;
+        KillCoro(ctx, reason);
+    }
+}
+
+void RpcCoroMgr::KillCoro(context &ctx, const std::string &reason) noexcept {
+    ctx.controller->SetFailed(reason);
+    ctx.handle.resume();
+    suspended_contexts_.erase(ctx.coro_uid);
+}
+
+void RpcCoroMgr::HandleCoroTimeout() {
+    llbc::sint64 now = llbc::LLBC_GetMilliseconds();
+    while (!coroHeap_.empty()) {
+        auto top = coroHeap_.top();
+        if (top.timeout_time > now) {
+            break;
+        }
+        coroHeap_.pop();
+        // already resumed
+        if (suspended_contexts_.find(top.coro_uid) == suspended_contexts_.end()) {
+            continue;
+        }
+        KillCoro(top, "coro timeout");
+    }
+}
+
+RpcCoroMgr::context RpcCoroMgr::PopCoroContext(coro_uid_type coro_uid) noexcept {
+    if (auto iter = suspended_contexts_.find(coro_uid);
+        iter != suspended_contexts_.end()) {
+        auto ctx = iter->second;
+        suspended_contexts_.erase(iter);
+        return ctx;
+    }
+    LLOG_ERROR("coro_uid not found|coro_uid:%lu", coro_uid);
+    return context{.handle = nullptr, .rsp = nullptr};
+}
--- src/rpc_coro_mgr.h
+++ src/rpc_coro_mgr.h
@@ -36,58 +36,18 @@ class RpcCoroMgr : public Singleton<RpcCoroMgr> {
 
     virtual ~RpcCoroMgr() = default;
 
-    int Init() { return 0; }
+    bool AddCoroContext(context ctx) noexcept;
+
+    void KillCoro(coro_uid_type coro_uid, const std::string &reason) noexcept;
+
+    void KillCoro(context &ctx, const std::string &reason) noexcept;
+
+    context PopCoroContext(coro_uid_type coro_uid) noexcept;
+
+    void HandleCoroTimeout();
 
     static coro_uid_type NewCoroUid() noexcept {
         return ++coro_uid_generator_ == 0UL ? ++coro_uid_generator_ : coro_uid_generator_;
-    }
-
-    bool AddCoroContext(context ctx) noexcept {
-        coroHeap_.push(ctx);
-        return suspended_contexts_.insert({ctx.coro_uid, ctx}).second;
-    }
-
-    void KillCoro(coro_uid_type coro_uid, const std::string &reason) noexcept {
-        if (auto iter = suspended_contexts_.find(coro_uid);
-            iter != suspended_contexts_.end()) {
-            auto ctx = iter->second;
-            ctx.controller->SetFailed(reason);
-            ctx.handle.resume();
-            suspended_contexts_.erase(iter);
-        }
-    }
-
-    void KillCoro(context &ctx, const std::string &reason) noexcept {
-        ctx.controller->SetFailed(reason);
-        ctx.handle.resume();
-        suspended_contexts_.erase(ctx.coro_uid);
-    }
-
-    context PopCoroContext(coro_uid_type coro_uid) {
-        if (auto iter = suspended_contexts_.find(coro_uid);
-            iter != suspended_contexts_.end()) {
-            auto ctx = iter->second;
-            suspended_contexts_.erase(iter);
-            return ctx;
-        }
-        LLOG_ERROR("coro_uid not found|coro_uid:%lu", coro_uid);
-        return context{.handle = nullptr, .rsp = nullptr};
-    }
-
-    void HandleCoroTimeout() {
-        llbc::sint64 now = llbc::LLBC_GetMilliseconds();
-        while (!coroHeap_.empty()) {
-            auto top = coroHeap_.top();
-            if (top.timeout_time > now) {
-                break;
-            }
-            coroHeap_.pop();
-            // already resumed
-            if (suspended_contexts_.find(top.coro_uid) == suspended_contexts_.end()) {
-                continue;
-            }
-            KillCoro(top, "coro timeout");
-        }
     }
 
     static constexpr int CORO_TIME_OUT = 10000;  // 10s
