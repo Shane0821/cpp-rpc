c2ffed3e7ca09983507a70e3808d1f30d3d00cb3
Shane <143796207+Shane0821@users.noreply.github.com>
2024-10-04 14:18:30 +0800

    upd spsc queue test

--- test/include_test/spsc_queue_test.cpp
+++ test/include_test/spsc_queue_test.cpp
@@ -1,10 +1,67 @@
-#include <gtest/gtest.h>
-
 #include "spsc_queue.h"
 
-TEST(SPSCQueueTest, PushPop) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+#include <gtest/gtest.h>
+
+struct Item {
+   public:
+    Item() = default;
+    Item(int a, int b) : a(a), b(b) {}
+
+    int a = 0;
+    int b = 0;
+};
+
+TEST(SPSCQueueTest, PushTest) {
+    SPSCQueue<Item, 1024> q;
+    for (int i = 0; i < 1023; ++i) {
+        EXPECT_TRUE(q.emplace(i, i + 1));
+    }
+    EXPECT_FALSE(q.emplace(1023, 1024));
+}
+
+TEST(SPSCQueueTest, PopTest) {
+    SPSCQueue<Item, 1024> q;
+    for (int i = 0; i < 1023; ++i) {
+        q.emplace(i, i + 1);
+    }
+    for (int i = 0; i < 1023; ++i) {
+        Item item;
+        EXPECT_TRUE(q.pop(item));
+        EXPECT_EQ(item.a, i);
+        EXPECT_EQ(item.b, i + 1);
+    }
+    Item item;
+    EXPECT_FALSE(q.pop(item));
+}
+
+TEST(SPSCQueueTest, SizeTest) {
+    SPSCQueue<Item, 1024> q;
+    for (int i = 0; i < 1023; ++i) {
+        EXPECT_TRUE(q.emplace(i, i + 1));
+        EXPECT_EQ(q.size(), i + 1);
+    }
+    EXPECT_FALSE(q.emplace(1023, 1024));
+    EXPECT_EQ(q.size(), 1023);
+
+    for (int i = 0; i < 1023; ++i) {
+        Item item;
+        EXPECT_TRUE(q.pop(item));
+        EXPECT_EQ(q.size(), 1023 - i - 1);
+    }
+}
+
+TEST(SPSCQueueTest, EmptyTest) {
+    SPSCQueue<Item, 1024> q;
+    EXPECT_EQ(q.empty(), true);
+    for (int i = 0; i < 1023; ++i) {
+        q.emplace(i, i + 1);
+        EXPECT_EQ(q.empty(), false);
+    }
+
+    for (int i = 0; i < 1023; ++i) {
+        EXPECT_EQ(q.empty(), false);
+        Item item;
+        q.pop(item);
+    }
+    EXPECT_EQ(q.empty(), true);
 }
