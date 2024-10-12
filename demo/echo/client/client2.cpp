5f7c7ef874dfd012105966aeb1be30d71c215457
Shane <143796207+Shane0821@users.noreply.github.com>
2024-10-12 20:20:34 +0800

    upd client2

---
+++ demo/echo/client/client2.cpp
@@ -0,0 +1,86 @@
+#include <memory>
+
+#include "echo.pb.h"
+#include "echo_service_stub.h"
+#include "polaris.h"
+#include "rpc_client.h"
+#include "rpc_controller.h"
+
+const char *SERVICE_NAME = "echo.EchoService.Echo";
+
+class EchoClient : public RpcClient {
+   public:
+    virtual RpcCoro CallMethod() override {
+        // create rpc req & resp
+        echo::EchoRequest req;
+        echo::EchoResponse rsp;
+
+        RpcChannel *channel =
+            RegisterRpcChannel(polaris::NameRegistry["echo.EchoService.RelayEcho"].ip,
+                               polaris::NameRegistry["echo.EchoService.RelayEcho"].port);
+
+        if (!channel) {
+            co_return;
+        }
+
+        std::unique_ptr<RpcController> cntl = std::make_unique<RpcController>(true);
+
+        cntl->SetCoroHandle(co_await GetHandleAwaiter{});
+
+        EchoServiceStub stub(channel);
+
+        req.set_msg("Hello, Echo.");
+        LLOG_INFO("EchoClient rpc echo call: msg:%s", req.msg().c_str());
+        co_await stub.RelayEcho(cntl.get(), &req, &rsp, nullptr);
+        LLOG_INFO("Recv Echo Rsp, status:%s, rsp:%s",
+                  cntl->Failed() ? cntl->ErrorText().c_str() : "success",
+                  rsp.msg().c_str());
+
+        co_return;
+    }
+
+    virtual void BlockingCallMethod() override {
+        // create rpc req & resp
+        echo::EchoRequest req;
+        echo::EchoResponse rsp;
+
+        RpcChannel *channel =
+            RegisterRpcChannel(polaris::NameRegistry["echo.EchoService.RelayEcho"].ip,
+                               polaris::NameRegistry["echo.EchoService.RelayEcho"].port);
+        if (!channel) {
+            return;
+        }
+
+        std::unique_ptr<RpcController> cntl = std::make_unique<RpcController>(false);
+        EchoServiceStub stub(channel);
+
+        req.set_msg("Hello, Echo.");
+        LLOG_INFO("EchoClient rpc echo call: msg:%s", req.msg().c_str());
+        stub.RelayEcho(cntl.get(), &req, &rsp, nullptr);
+        LLOG_INFO("Recv Echo Rsp, status:%s, rsp:%s",
+                  cntl->Failed() ? cntl->ErrorText().c_str() : "success",
+                  rsp.msg().c_str());
+    }
+};
+
+int main() {
+    EchoClient client;
+    client.Init();
+
+    const std::string path = __FILE__;
+    const std::string CLIENT_LLOG_CONF_PATH =
+        path.substr(0, path.find_last_of("/\\")) + "/../../../config/client_log.cfg";
+    client.SetLogConfPath(CLIENT_LLOG_CONF_PATH.c_str());
+
+    // LLOG_TRACE("CallMeathod Start");
+    // client.CallMethod();
+    // LLOG_TRACE("CallMeathod return");
+    // for (int i = 0; i < 20000; i++) {
+    //     client.Update();
+    // }
+
+    LLOG_TRACE("BlockingCallMethod Start");
+    client.BlockingCallMethod();
+    LLOG_TRACE("BlockingCallMethod return");
+    return 0;
+}
