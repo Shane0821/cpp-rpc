5d80e2f52547d23637fd6b8583cfa143d63077cf
Shane <143796207+Shane0821@users.noreply.github.com>
2024-11-18 23:26:41 +0800

    use :

--- src/rpc_registry.cpp
+++ src/rpc_registry.cpp
@@ -4,6 +4,7 @@
 
 int RpcRegistry::Connect(const std::string &url) {
     auto ret = client_->connect(url);
+    client_->set_log_lvl(utility::zoo_log_lvl_error);
     COND_RET_ELOG(ret != utility::z_ok, LLBC_FAILED,
                   "RpcRegistry Connect failed, url: %s", url.c_str());
     return LLBC_OK;
@@ -13,17 +14,15 @@ int RpcRegistry::RegisterService(const std::string &svc_md, const std::string &a
     std::vector<utility::zoo_acl_t> acl;
     acl.push_back(utility::zk_cpp::create_world_acl(utility::zoo_perm_all));
     auto path = "/" + svc_md;
-    auto ret = client_->create_ephemeral_node(path.c_str(), "", acl);
+    auto ret = client_->create_persistent_node(path.c_str(), "", acl);
     COND_RET_ELOG(ret != utility::z_node_exists && ret != utility::z_ok, LLBC_FAILED,
-                  "RegisterService failed, svc_md: %s, addr: %s", svc_md.c_str(),
-                  addr.c_str());
+                  "RegisterService failed, svc_md: %s, addr: %s, ret: %d", svc_md.c_str(),
+                  addr.c_str(), ret);
+
     ret = client_->create_ephemeral_node((path + "/" + addr).c_str(), "", acl);
-    COND_RET_ELOG(ret == utility::z_node_exists, LLBC_FAILED,
-                  "RegisterService failed, addr exsists, svc_md: %s, addr: %s",
-                  svc_md.c_str(), addr.c_str());
-    COND_RET_ELOG(ret != utility::z_ok, LLBC_FAILED,
-                  "RegisterService failed, svc_md: %s, addr: %s", svc_md.c_str(),
-                  addr.c_str());
+    COND_RET_ELOG(ret != utility::z_node_exists && ret != utility::z_ok, LLBC_FAILED,
+                  "RegisterService failed, svc_md: %s, addr: %s, ret: %d", svc_md.c_str(),
+                  addr.c_str(), ret);
 
     return LLBC_OK;
 }
@@ -57,7 +56,7 @@ RpcRegistry::ServiceAddr RpcRegistry::ParseServiceAddr(const std::string &svc_md
     // remove svc_md prefix
     auto tmp = path.substr(svc_md.size() + 1);
     // parse ip:port
-    auto pos = tmp.find('.');
+    auto pos = tmp.find(':');
     if (pos == std::string::npos) {
         return {};
     }
